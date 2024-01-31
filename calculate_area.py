d32454602ce2fae65277f2af8a1d3ede457f723b
devensharma192001 <90694451+devensharma192001@users.noreply.github.com>
2024-01-31 19:00:44 +0530

    Add files via upload

---
+++ calculate_area.py
@@ -0,0 +1,15 @@
+def calculate_area(length, width):
+    if length == width:
+        return "This is a square!"
+    else:
+        return length * width
+
+def main():
+    length = float(input("Enter the length: "))
+    width = float(input("Enter the width: "))
+    
+    result = calculate_area(length, width)
+    print("Area:", result)
+
+if __name__ == "__main__":
+    main()
